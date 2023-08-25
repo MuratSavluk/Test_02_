134c9153b9222657882cf218620953d3f3bdb2ed
Bavudun <106803325+abdurahim0201@users.noreply.github.com>
2023-08-25 21:23:21 +0300

    My fun

--- src/Utility/MyFun.java
+++ src/Utility/MyFun.java
@@ -1,4 +1,11 @@
 package Utility;
 
 public class MyFun {
+    public static void Wait(int sn){
+        try {
+            Thread.sleep(sn * 1000l);
+        }catch (InterruptedException e){
+            throw new RuntimeException(e);
+        }
+    }
 }
