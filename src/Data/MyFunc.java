45cfbdcd446c9b251f45e8c1becd962c75cffcb4
murat <MuratSavluk1@gmail.com>
2023-08-30 14:34:27 -0500

    Biten Görev

---
+++ src/Data/MyFunc.java
@@ -0,0 +1,15 @@
+package Data;
+
+public class MyFunc {
+
+    public static void Bekle(int sn){
+
+        try {
+            Thread.sleep(sn*1000l);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+
+    }
+
+}
---
+++ src/Data/OturumKapatma.java
@@ -0,0 +1,53 @@
+package Data;
+
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.WebElement;
+
+
+
+       // 1-  Bir kullanıcı olarak, web mağazamdan hesabımdan çıkış yapabilmeliyim. Böylece alışverişim daha güvenli olur.
+      //  2-  Kullanıcı hesaptan çıkış yapabilmelidir. Kullanıcı oturumu başarıyla kapattığı zaman giriş yapma sayfasına yönlendirilmelidir.
+      // 3-   Kullanıcı web sitesine giriş yapmış durumda olmalıdır.
+
+public class OturumKapatma extends BaseDriver {
+    @Test
+    public void test(){
+
+        driver.get("https://demowebshop.tricentis.com/");
+
+        WebElement LoginBtn = driver.findElement(By.cssSelector("[class='ico-login']"));
+        LoginBtn.click();
+        MyFunc.Bekle(2);
+
+        WebElement mail = driver.findElement(By.id("Email"));
+        mail.sendKeys("technostudy13@gotmail.com");
+        WebElement pswrd = driver.findElement(By.id("Password"));
+        pswrd.sendKeys("technostudy13");
+
+        WebElement girisYap = driver.findElement(By.xpath("//input[@class='button-1 login-button']"));
+        MyFunc.Bekle(2);
+        girisYap.click();
+
+        WebElement cikis = driver.findElement(By.xpath("//a[@class='ico-logout']"));
+        MyFunc.Bekle(1);
+        cikis.click();
+
+        WebElement LoginGrs = driver.findElement(By.cssSelector("[class='ico-login']"));
+        LoginGrs.click();
+        MyFunc.Bekle(2);
+
+        WebElement LoginAnaSayfa = driver.findElement(By.cssSelector("[class='ico-login']"));
+        MyFunc.Bekle(2);
+        LoginAnaSayfa.click();
+
+        MyFunc.Bekle(1);
+        bekleVeKapat();
+
+
+
+    }
+
+
+}
